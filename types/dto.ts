8b91aa7624b84d18eb702c5966155a96e42331f8
benjahuenchunir <bahuenchunir@gmail.com>
2024-07-16 12:11:16 -0400

    chore: migrate to nuxt

--- types/dto.ts
+++
@@ -1,7 +0,0 @@
-import { createSupabaseServerClient } from "@/lib/supabase/server";
-import { QueryData } from "@supabase/supabase-js";
-
-const supabase = createSupabaseServerClient();
-const query = supabase.from("cursos").select("*, secciones(*)").eq("sigla", "example").single();
-
-export type CourseDTO = QueryData<typeof query>;
